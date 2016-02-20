b642ff76a11a91f064e66a1c498647302f60efc2
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-02-20 12:08:45 -0500

    Changed methodname, globalized 2 variables, & cleaned code

--- Adventure Text Game/Adventure2.java
+++ Adventure Text Game/Adventure2.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class Adventure2{
 	private static int nextroom = 1;
-	public static void main( String[] args ){
-		Scanner keyboard = new Scanner(System.in);
-		
-		// int nextroom = 1;
-		String choice = "", yesno;
+	private static String choice = "", yesno;
 
+	public static void main( String[] args ){
+				
 		while (nextroom != 0){
 
 			// The Haunted House Hallway Selection
 			if (nextroom == 1){
 				System.out.println("\nYou are in a haunted house! Would you like to go 'upstairs' or into the 'kitchen'?");
 				System.out.println("To end the adventure now, say 'end'");
-				choice = keyboard.nextLine();
 				userChoice(choice);
 			}
 
@@ -24,7 +21,6 @@ public class Adventure2{
 				System.out.println("You see a hallway. At the end of the hallway is the master 'bedroom'."); 
 				System.out.println("You also notice a 'bathroom' off the hallways. Where would you like to go?");
 				System.out.println("If you're too scared, go 'back'");
-				choice = keyboard.nextLine();
 				userChoice(choice);
 			}
 			
@@ -35,7 +31,6 @@ public class Adventure2{
 				System.out.println("Off to the side, there is, as you'd expect, a refrigerator.");
 				System.out.println("You may open the 'fridge' or look in a 'cabinet'");
 				System.out.println("If you're too scared, go 'back'");
-				choice = keyboard.nextLine();
 				userChoice(choice);
 			}
 			
@@ -44,8 +39,7 @@ public class Adventure2{
 			{
 				System.out.println("Inside the refrigerator you see mouldy food.");
 				System.out.println("Would you like a bite? ['yes' or 'no']");
-				yesno = keyboard.next();
-				yesNo(yesno);
+				select(yesno);
 
 			}
 
@@ -54,8 +48,7 @@ public class Adventure2{
 			{
 				System.out.println("A hidden ghoul emergences from the cabinet!");
 				System.out.println("Will you 'run' or 'stay'?");
-				yesno = keyboard.next();
-				yesNo(yesno);
+				select(yesno);
 			}
 
 			// User at bedroom (upstairs)
@@ -64,8 +57,7 @@ public class Adventure2{
 				System.out.println("You are in a plush bedroom, with expensive-looking hardwood furniture.");
 				System.out.println("The bed is unmade. In the back of the room, the closet door is ajar.");
 				System.out.println("Would you like to open the door? ['yes' or 'no']");
-				yesno = keyboard.next();
-				yesNo(yesno);
+				select(yesno);
 			}
 
 			// User at bathroom (upstairs)
@@ -74,8 +66,7 @@ public class Adventure2{
 				System.out.println("You are in a dirty bathroom.");
 				System.out.println("The wallpaper is coming off. The sink is running a brown water.");
 				System.out.println("Suddenly you're thirsty. Do you drink it? ['yes' or 'no']");
-				yesno = keyboard.next();
-				yesNo(yesno);
+				select(yesno);
 			}
 		}
 
@@ -83,8 +74,16 @@ public class Adventure2{
 	}
 
 	public static void userChoice(String choice){
+		Scanner keyboard = new Scanner(System.in);
+		choice = keyboard.nextLine();
+
+		// Return to Haunted House Hallway
+		if ( choice.equals("back") ){
+			nextroom = 1;
+		}
+
 		// User Chooses to go Upstairs
-		if (choice.equals("upstairs")){
+		else if (choice.equals("upstairs")){
 			nextroom = 2;
 		}
 		
@@ -93,15 +92,13 @@ public class Adventure2{
 			nextroom = 3;
 		}
 		
-		// User leaves game by choice
-		else if (choice.equals("end")){
-			System.out.println("You chose to end the adventure and leave. Sad, really.");
-			nextroom = 0;
+		// User is in the downstairs Kitchen
+		else if (choice.equals("fridge")){
+			nextroom = 4;
 		}
-
-		// Return to Haunted House Hallway
-		else if ( choice.equals("back") ){
-			nextroom = 1;
+		
+		else if (choice.equals("cabinet")){
+			nextroom = 5;
 		}
 
 		// User Decisions in Bedroom (Which is located upstairs)
@@ -114,20 +111,20 @@ public class Adventure2{
 			nextroom = 7;
 		}
 
-		else if (choice.equals("fridge")){
-			nextroom = 4;
+		// User leaves game by choice
+		else if (choice.equals("end")){
+			System.out.println("You chose to end the adventure and leave. Sad, really.");
+			nextroom = 0;
 		}
-		
-		else if (choice.equals("cabinet")){
-			nextroom = 5;
-		}
-		
+
 		else{
 			System.out.println( "ERROR." );
 		}
 	}
 
-	public static void yesNo(String yesno){
+	public static void select(String yesno){
+		Scanner keyboard = new Scanner(System.in);
+		yesno = keyboard.next();
 
 		if (yesno.equals("yes")){
 
@@ -150,6 +147,7 @@ public class Adventure2{
 				nextroom = 0;
 			}
 		}
+		
 		else if (yesno.equals("no")){
 
 			//Starvation
@@ -170,10 +168,12 @@ public class Adventure2{
 				nextroom = 0;
 			}
 		}
+		
 		else if (yesno.equals("run")){
 			System.out.println("You've escaped from the haunted house! Until next time...");
 			nextroom = 0;
 		}
+		
 		else if (yesno.equals("stay")){
 			System.out.println("The ghoul laughs maniacally. You are captured under his dark magic for eternity.");
 			nextroom = 0;

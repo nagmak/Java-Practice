8a56450df1f643b47a81a69bfaf7d7b3965b77ba
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-02-21 22:51:37 -0500

    Added pickShape method

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -1,34 +1,44 @@
-//Area Calculator
+// AREA CALCULATOR
 // Presents menu to select shape to be calculated
 // ask for respective values for calculation
 // pass values to function, and display area
+
 import java.util.Scanner;
 
 public class AreaCalculator{
 
 	//Main Program to call the other functions
 	public static void main(String[] args){
-
 		Scanner keyboard = new Scanner(System.in);
 		int shape = 0;
+
 		System.out.println("Shape Area Calculator Version 1.0 (c) 2016 Nagma K.\n");
 	
-	while (shape !=5){
-		System.out.println("------------------------------\n");
-		System.out.println("1) Circle");
-		System.out.println("2) Rectangle");
-		System.out.println("3) Square");
-		System.out.println("4) Triangle");
-		System.out.println("5) Quit");
-		System.out.print("\nWhich shape: ");
-		shape = keyboard.nextInt();
+		while (shape !=5){
+			System.out.println("------------------------------\n");
+			System.out.println("1) Circle");
+			System.out.println("2) Rectangle");
+			System.out.println("3) Square");
+			System.out.println("4) Triangle");
+			System.out.println("5) Quit");
+			System.out.print("\nWhich shape: ");
+			
+			shape = keyboard.nextInt();
+			pickShape(shape);
+		}
 
-	
+	}
+
+	// User shape selection
+	public static void pickShape(int shape){
+		Scanner keyboard = new Scanner(System.in);
+
 		if (shape == 1){
 			System.out.print("Radius: ");
 			double radius = keyboard.nextDouble();
 			System.out.println("The area of the circle is: " + area_circle(radius));
 		}
+
 		else if (shape == 2){
 			System.out.print("Length: ");
 			int length = keyboard.nextInt();
@@ -36,11 +46,13 @@ public class AreaCalculator{
 			int width = keyboard.nextInt();
 			System.out.println("The area of the rectangle is: " + area_rectangle(length, width));
 		}
+
 		else if (shape == 3){
 			System.out.print("Side: ");
 			int side = keyboard.nextInt();
 			System.out.println("The area of the square is: " + area_square(side));
 		}
+
 		else if (shape == 4){
 			System.out.print("Base: ");
 			double base = keyboard.nextDouble();
@@ -48,15 +60,14 @@ public class AreaCalculator{
 			double height = keyboard.nextDouble();
 			System.out.println("The area of the triangle is: " + area_triangle(base, height));
 		}
+
 		else if (shape == 5){
 			System.out.println("Goodbye.");
-			break;
 		}
+
 		else{
 			System.out.println("ERROR.");
 		}
-	}
-
 	}
 
 	//Calculates area of circle

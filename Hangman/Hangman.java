012332555e6cb458f30dca14e3a090edb41a5016
NAGMA KAPOOR <nagmakapoor@gmail.com>
2016-03-31 14:27:05 -0400

    Re-started problem; need to use StringBuilder

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -3,68 +3,47 @@
 //Stops when all letters are guessed
 //Gives a limited # of tries and stops once they run out
 //Displays letters that have been guessed already, incorrect or all guesses
+// Consider using StringBuilder
 
 import java.util.Scanner;
 import java.util.Random;
 
 public class Hangman{
-	private static char cguess = '\0';
-	private static String guess = "";
-
 	public static void main(String[] args){
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("--HANGMAN--\n");
-		String[] word = {"water", "granger", "magical", "voldemort"};
+		String[] words = {"ocean", "cafe", "waterfall"};
 		Random r = new Random();
-		int choice = r.nextInt(4);
-		//System.out.println(choice);
-		int user = 1;
-
-		while (user != 0){
-			
-
-			// System.out.println("\n\nMisses: ");
-			// System.out.println("\nGuess: \n" + "------\n");
-
-			cguess = makeGuess(choice, word);
-			user = updateHangman(choice, word, user);
-
-			
-		}
-		
-	}
-
-	public static char makeGuess(int choice, String[] word){
 		Scanner keyboard = new Scanner(System.in);
-		//Intialize board
-		System.out.print("Word: ");
 
-		for (int i =0; i<word[choice].length()-1; i++){
+		// Random selection of a Hangman word from the words array
+		String wordSelect = words[r.nextInt(words.length)];
+		System.out.print("\nWord: ");
+		for (int i = 0; i < wordSelect.length(); i++){
 			System.out.print("_ ");
 		}
-		
-		System.out.println("\n\nMisses: ");
-		System.out.println("\nGuess: ");
-		guess = keyboard.next();
-		cguess = guess.charAt(0); 
 
-		return cguess;
-	}
+		System.out.print("\nMisses: ");
+		//System.out.print("\nGuess: \n");
 
-	public static int updateHangman(int choice, String[] word, int user){
-		System.out.print("Word: ");
-		for (int a = 0; a<word[choice].length()-1; a++){
-			if (cguess == word[choice].charAt(a)){
-				System.out.print(guess + " ");
-			}
-			else{
-				System.out.print("_ ");
-			}
+		// Guess attempts
+		int count = 0;
+
+		while (count != 4){
+			System.out.print("\nGuess: ");
+			char guess = keyboard.next().trim().charAt(0);
 			
-			user = 0;
+			System.out.print("\nWord: ");
+			
+			for (int i = 0; i<wordSelect.length(); i++){
+				if (guess == wordSelect.charAt(i)){
+					System.out.print(wordSelect.charAt(i));
+				}
+				else {
+					System.out.print("_ ");
+					count++;
+				}
+			}
+
 		}
-		
-		System.out.println("\n\nMisses: ");
-		return user;
 	}
+	
 }
